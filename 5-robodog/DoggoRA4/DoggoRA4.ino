e76ee6011acb5461dadc3be89b103a5710cfd1e8
Alexandra <alexandra@zalmotek.com>
2024-07-08 16:12:00 +0300

    slightly working walking code

--- 5-robodog/DoggoRA4/DoggoRA4.ino
+++ 5-robodog/DoggoRA4/DoggoRA4.ino
@@ -12,6 +12,9 @@ void setup() {
     Serial.println("Failed to init soft serial");
   }
 
+  // Initial pose - lower and leaning more forward
+  moveBody(0.0, 0.0, 150.0, -0.1, 0.0);
+  delay(5000);
   Serial.println(SoftSerial);
 }
 
@@ -28,127 +31,55 @@ String incomingText;
 
 void loop() {  // run over and over
   if (SoftSerial.available() > 0) {
-    sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", 0.0, 0.0, 90.0, 0.0, 0.0, 0.0, 40.0, 40.0);
-    Serial.write(cstr);
-    SoftSerial.write(cstr);
-    delay(3000);
+    // Step 1: Lift and move forward diagonal pair (front-left and rear-right)
+    moveLeg("fl", 0.0, 150.0, -50.0);
+    moveLeg("rr", 0.0, 150.0, -50.0);
+    moveLeg("fr", 0.0, 0.0, -50.0);
+    moveLeg("rl", 0.0, 0.0, -50.0);
 
-    sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", 0.0, 0.0, 120.0, 0.1, 0.0, 0.0, 60.0, 60.0);
-    Serial.write(cstr);
-    SoftSerial.write(cstr);
-    delay(300);
+    // Step 2: Lower the lifted legs
+    moveLeg("fl", 0.0, 0.0, 20.0);
+    moveLeg("rr", 0.0, 0.0, 20.0);
+    moveLeg("fr", 0.0, 0.0, 20.0);
+    moveLeg("rl", 0.0, 0.0, 20.0);
 
-    sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", 0.0, 0.0, 160.0, 0.2, 0.0, 0.0, 80.0, 80.0);
-    Serial.write(cstr);
-    SoftSerial.write(cstr);
-    delay(300);
+    // Step 3: Shift body weight forward
+    moveBody(0.0, -50.0, 150.0, 0.2, 0.0);
 
-    sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", 0.0, 0.0, 160.0, 0.3, 0.0, 0.0, 80.0, 80.0);
-    Serial.write(cstr);
-    SoftSerial.write(cstr);
-    delay(300);
+    // Step 4: Lift and move forward other diagonal pair (front-right and rear-left)
+    moveLeg("fr", 0.0, 150.0, -50.0);
+    moveLeg("rl", 0.0, 150.0, -50.0);
+    moveLeg("fl", 0.0, 0.0, -50.0);
+    moveLeg("rr", 0.0, 0.0, -50.0);
 
-    sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", 0.0, 0.0, 160.0, 0.4, 0.0, 0.0, 80.0, 80.0);
-    Serial.write(cstr);
-    SoftSerial.write(cstr);
-    delay(300);
+    // Step 5: Lower the lifted legs
+    moveLeg("fr", 0.0, 0.0, 20.0);
+    moveLeg("rl", 0.0, 0.0, 20.0);
+    moveLeg("fl", 0.0, 0.0, 20.0);
+    moveLeg("rr", 0.0, 0.0, 20.0);
 
-    sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", 0.0, 0.0, 160.0, 0.5, 0.0, 0.0, 80.0, 80.0);
-    Serial.write(cstr);
-    SoftSerial.write(cstr);
-    delay(300);
+    // Step 6: Complete body shift
+    moveBody(0.0, -50.0, 150.0, 0.2, 0.0);
 
-    sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", 0.0, 0.0, 160.0, 0.6, 0.0, 0.0, 80.0, 80.0);
-    Serial.write(cstr);
-    SoftSerial.write(cstr);
-    delay(300);
+    // Step 7: Reset body position for next cycle
+    moveBody(0.0, 0.0, 150.0, -0.1, 0.0);
 
-    sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", 0.0, 0.0, 160.0, 0.5, 0.0, 0.0, 80.0, 80.0);
-    Serial.write(cstr);
-    SoftSerial.write(cstr);
-    delay(300);
-
-    sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", 0.0, 0.0, 160.0, 0.4, 0.0, 0.0, 80.0, 80.0);
-    Serial.write(cstr);
-    SoftSerial.write(cstr);
-    delay(300);
-
-    sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", 0.0, 0.0, 160.0, 0.3, 0.0, 0.0, 80.0, 80.0);
-    Serial.write(cstr);
-    SoftSerial.write(cstr);
-    delay(300);
-
-    sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", 0.0, 0.0, 160.0, 0.2, 0.0, 0.0, 80.0, 80.0);
-    Serial.write(cstr);
-    SoftSerial.write(cstr);
-    delay(300);
-
-    sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", 0.0, 0.0, 160.0, 0.1, 0.0, 0.0, 80.0, 80.0);
-    Serial.write(cstr);
-    SoftSerial.write(cstr);
-    delay(300);
+    delay(50);  // Short pause before next cycle
   }
 }
 
+void moveLeg(const char* leg, float x, float y, float z) {
+  char cstr[50];
+  sprintf(cstr, "F%s tx%.3f ty%.3f tz%.3f \n", leg, x, y, z);
+  Serial.write(cstr);
+  SoftSerial.write(cstr);
+  delay(50);
+}
 
-  // if (Serial.available() > 0) {
-  //   incomingText = Serial.readString();  // read the incoming byte:
-  //   Serial.println(incomingText);
-
-  //   // Split the string into substrings
-  //   while (incomingText.length() > 0) {
-  //     int index = incomingText.indexOf(' ');
-  //     if (index == -1)  // No space found
-  //     {
-  //       strs[StringCount++] = incomingText;
-  //       break;
-  //     } else {
-  //       strs[StringCount++] = incomingText.substring(0, index);
-  //       incomingText = incomingText.substring(index + 1);
-  //     }
-  //   }
-  //   StringCount = 0;
-
-  //   tx = strs[0].toFloat();
-  //   ty = strs[1].toFloat();
-  //   tz = strs[2].toFloat();
-
-  //   rx = strs[3].toFloat();
-  //   ry = strs[4].toFloat();
-  //   rz = strs[5].toFloat();
-
-  //   sx = strs[6].toFloat();
-  //   sy = strs[7].toFloat();
-
-  //   sprintf(cstr, "Ffl tx%.3f ty%.3f tz%.3f \n", tx, ty, tz);
-  //   sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", tx, ty, tz, rx, ry, rz,
-  //           sx, sy);
-  //   Serial.write(cstr);
-  // }
-
-  // tx = 0;     // max 210
-  // ty = 0;     // max 210
-  // tz = 90;    // max 210
-  // rx = 0;     // max 1 - se apleaca sa manance
-  // ry = 0;     // max 1 - se apleaca pe picior stanga dreapta
-  // rz = 0;     // max 1 -  se uita peste umar stanga dreapta
-  // sx = 90;    // max 120
-  // sy = 90;    // max 120
-
-  // sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", tx, ty, tz, rx, ry, rz,
-  //         sx, sy);
-
-// String line = SoftSerial.read();
-// line.trim();
-
-// char buffer[1024];
-// line.toCharArray(buffer, sizeof(buffer));
-
-// if (buffer[0] == 'F' && buffer[1] == 'F') {
-//   float flx, fly, flz, frx, fry, frz, rrx, rry, rrz, rlx, rly, rlz;
-//   sscanf(buffer, "FF flx%f fly%f flz%f frx%f fry%f frz%f rrx%f rry%f rrz%f rlx%f rly%f rlz%f", &flx, &fly,
-//          &flz, &frx, &fry, &frz, &rrx, &rry, &rrz, &rlx, &rly, &rlz);
-// }
-
-// Serial.print("pico says: ");
-// Serial.println(SoftSerial.read());
+void moveBody(float x, float y, float z, float rx, float ry) {
+  char cstr[100];
+  sprintf(cstr, "P tx%.3f ty%.3f tz%.3f rx%.3f ry%.3f rz%.3f sx%.3f sy%.3f\n", x, y, z, rx, ry, 0.0, 100.0, 100.0);
+  Serial.write(cstr);
+  SoftSerial.write(cstr);
+  delay(50);
+}
